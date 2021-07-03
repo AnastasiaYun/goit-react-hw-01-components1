94435fc9e2b82fbe0dd8832065cd712c45e92a1a
AnastasiaYun <tigrawom@gmail.com>
2021-07-04 01:50:04 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Friends from "./components/Friend-list/Friends-list";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Friends></Friends>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Friend-list/Friends-list.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Friend-list.style";
+
+const Friends = () => {
+  return <Container></Container>;
+};
+
+export default Friends;
