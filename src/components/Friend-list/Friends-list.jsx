658f1068e4568979d3ea8f2a367f478ebb1e91f5
AnastasiaYun <tigrawom@gmail.com>
2021-07-08 01:13:15 +0300

    1

--- src/components/Friend-list/Friends-list.jsx
+++ src/components/Friend-list/Friends-list.jsx
@@ -7,21 +7,6 @@ import {
   Offline,
 } from "./Friend-list.style";
 
-function Friends({ friends }) {
-  return (
-    <Container>
-      <Span>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <ItemFriends key={id}>
-            {isOnline ? <Online></Online> : <Offline></Offline>}
 
-            <img src={avatar} alt="{name}" width="48" />
-            <p>{name}</p>
-          </ItemFriends>
-        ))}
-      </Span>
-    </Container>
-  );
-}
 
 export default Friends;
